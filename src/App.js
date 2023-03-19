819225fb255cc333d60f2f3839e24e127a2b0b7e
Arvind <thakurarvindkr10@gmail.com>
2023-03-19 17:28:01 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
-import Profile from './Profile.js'
+import React, { useState } from 'react';
+import User from './User.js';
+import Members from './Member.js';
 
-function App(){
-  return(
+function App() {
+  function getData() {
+    alert('Hello from app')
+  }
+  return (
     <div className='App'>
-      <h2>Conditional Rendering If Condition</h2>
-      <Profile/>
+      {/* <h2 style={{backgroundColor:'yellow'}}>Pass function as Props</h2> */}
+      <User data={getData} />
+      <User data={getData} />
+      <User data={getData} />
+      <User data={getData} />
+      <User data={getData} />
+      <div style={{float:'left', backgroundColor:'yellow', padding:'20px'}}>
+        <Members data={getData} />
+      </div>
+
     </div>
   )
 }
