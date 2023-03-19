2e78313aa16e44edf2551626f466954c40e66862
Arvind <thakurarvindkr10@gmail.com>
2023-03-19 17:49:13 +0530

    Life Cycles Methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-import User from './User.js';
-import Members from './Member.js';
+
+// There are 3 Phase 
+/* 
+   1) Mounting 
+   2) Updating
+   3) UnMounting
+
+   When Life cycle methods invokes
+    when component is loaded
+    when component is updated with state and products
+    when component is removed
+ */
 
 function App() {
-  function getData() {
-    alert('Hello from app')
-  }
   return (
     <div className='App'>
-      {/* <h2 style={{backgroundColor:'yellow'}}>Pass function as Props</h2> */}
-      <User data={getData} />
-      <User data={getData} />
-      <User data={getData} />
-      <User data={getData} />
-      <User data={getData} />
-      <div style={{float:'left', backgroundColor:'yellow', padding:'20px'}}>
-        <Members data={getData} />
-      </div>
-
+      <h1>Life Cycle Methods</h1>
+      <h2>Products</h2><br/><br/>
+      <button>Load Product</button><br/><br/>
+      <button>Update Product</button><br/><br/>
+      <button>Remove Product</button>
     </div>
   )
 }
