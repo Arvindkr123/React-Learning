20ebd881498420533901365a4c66964360ca59a4
Arvind <thakurarvindkr10@gmail.com>
2023-03-20 16:06:25 +0530

    constructor life  cyle method

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 
-// There are 3 Phase 
-/* 
-   1) Mounting 
-   2) Updating
-   3) UnMounting
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     console.log('Constructor called');
+//   }
 
-   When Life cycle methods invokes
-    when component is loaded
-    when component is updated with state and products
-    when component is removed
- */
+//   render() {
+//     console.log('render called');
+//     return (
+//       <div className='App'>
+//         <h1>Constructor Life Cycle Methods</h1>
+//         <h1>Hello World ! </h1>
+//       </div>
+//     )
+//   }
+// }
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data :'Arvind'
+    }
+  }
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Life Cycle Methods</h1>
-      <h2>Products</h2><br/><br/>
-      <button>Load Product</button><br/><br/>
-      <button>Update Product</button><br/><br/>
-      <button>Remove Product</button>
-    </div>
-  )
+  render() {
+    return (
+      <div className='App'>
+        <h1>Constructor Life Cycle Methods</h1>
+        <h1>Hello World ! {this.state.data} </h1>
+      </div>
+    )
+  }
 }
+
+
 export default App; 
