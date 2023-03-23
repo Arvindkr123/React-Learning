f726e08e7bf0e05af06ad41ba88aee0771195957
Arvind <thakurarvindkr10@gmail.com>
2023-03-24 05:04:33 +0530

    Use Hocks in React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import Student from './Student';
-import React from 'react';
+import React, { useState } from 'react';
 
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      show:true
-    }
-  }
-  render() {
-    return (
-      <div className='App'>
-        {/* <h1>Component will unmount</h1> */}
-        {
-          this.state.show?<Student/>:<h1>Child Component removed</h1>
-        }
-        <button onClick={()=>{this.setState({show:!this.state.show
-        })}}>Toggle child Component</button>
-      </div >
-    )
-  }
+function App() {
+  const[data, setData] = useState('Arvind Kumar')
+  return (
+    <div className='App'>
+      <h1>{data}</h1>
+      <button onClick={()=>setData('Arvind Thakur')}>Update Name</button>
+    </div>
+  )
 }
+
+
+
+
+// function App() {
+//   const[data, setData] = useState('Anil');
+//   return (
+//     <div className='App'>
+//       <h1>Hocks in React</h1>
+//       <p>When we were using the class componet abd there would be a lot of inbuilt feature would there such as life cycle pure component if you want to use these the you have to use the <em>Hocks</em></p>
+//       <h3>Hocks can be used in the functional component</h3>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   const[data, setData] = useState('Anil');
+//   return (
+//     <div className='App'>
+//       <h1>Hocks in React</h1>
+//       <p>When we were using the class componet abd there would be a lot of inbuilt feature would there such as life cycle pure component if you want to use these the you have to use the <em>Hocks</em></p>
+//       <h3>Hocks can be used in the functional component</h3>
+//     </div>
+//   )
+// }
 
 export default App; 
