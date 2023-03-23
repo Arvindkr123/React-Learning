647df1b52bad189a82d6b5ac173d9dfc0df01f00
Arvind <thakurarvindkr10@gmail.com>
2023-03-23 22:41:17 +0530

    Should component update

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       count: 0
     }
-    console.log('Constructor called')
-
   }
-  componentDidUpdate(preProps, PreState, snapshot) {
-    console.log('componentDidUpdate called', PreState.count, this.state.count, snapshot)
-    if(this.state.count < 10){
-      this.setState({count:this.state.count+1})
+  componentDidUpdate(){
+    console.log('componentDidUpdate')
+  }
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate')
+    if (this.state.count >= 5 && this.state.count < 10) {
+      return true;
     }
   }
   render() {
-    console.log('render called')
     return (
       <div className='App'>
-        <h1>ComponentDidUpdate life Cycle Method {this.state.count}</h1>
-        <button onClick={() => { this.setState({ count:1}) }}>Update Name</button>
-      </div>
+        <h1>Should Component Update {this.state.count}</h1>
+        <button onClick={() => { this.setState({ count: this.state.count + 1 }) }}>Update Count</button>
+      </div >
     )
   }
 }
@@ -34,44 +33,17 @@ class App extends React.Component {
 //     this.state = {
 //       count: 0
 //     }
-//     console.log('Constructor called')
-
 //   }
-//   componentDidUpdate(preProps, PreState, snapshot) {
-//     console.log('componentDidUpdate called', PreState.count, this.state.count)
-//     if(this.state.count=== PreState.count){
-//       alert('data is already same')
-//     }
+//   shouldComponentUpdate(){
+//     console.log('shouldComponentUpdate', this.state.count)
+//     return true;
 //   }
 //   render() {
-//     console.log('render called')
 //     return (
 //       <div className='App'>
-//         <h1>ComponentDidUpdate life Cycle Method {this.state.count}</h1>
-//         <button onClick={() => { this.setState({ count:1}) }}>Update Name</button>
-//       </div>
-//     )
-//   }
-// }
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       name: 'Arvind'
-//     }
-//     console.log('Constructor called')
-
-//   }
-//   componentDidUpdate() {
-//     console.log('componentDidUpdate called')
-//   }
-//   render() {
-//     console.log('render called')
-//     return (
-//       <div className='App'>
-//         <h1>ComponentDidUpdate life Cycle Method {this.state.name}</h1>
-//         <button onClick={() => { this.setState({ name: 'Arvind Thakur' }) }}>Update Name</button>
-//       </div>
+//         <h1>Should Component Update {this.state.count}</h1>
+//         <button onClick={()=>{this.setState({count:this.state.count+1})}}>Update Count</button>
+//       </div >
 //     )
 //   }
 // }
