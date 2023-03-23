508b24223d479dcaa30863d013d097db21689ad9
Arvind <thakurarvindkr10@gmail.com>
2023-03-24 05:17:18 +0530

    use Effect

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
 
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const[data, setData] = useState('Arvind Kumar')
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('use Effect')
+  })
   return (
     <div className='App'>
-      <h1>{data}</h1>
-      <button onClick={()=>setData('Arvind Thakur')}>Update Name</button>
+      <h1>Use Effect in React {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Update Count</button>
     </div>
   )
 }
 
-
-
-
-// function App() {
-//   const[data, setData] = useState('Anil');
-//   return (
-//     <div className='App'>
-//       <h1>Hocks in React</h1>
-//       <p>When we were using the class componet abd there would be a lot of inbuilt feature would there such as life cycle pure component if you want to use these the you have to use the <em>Hocks</em></p>
-//       <h3>Hocks can be used in the functional component</h3>
-//     </div>
-//   )
-// }
-
-// function App() {
-//   const[data, setData] = useState('Anil');
-//   return (
-//     <div className='App'>
-//       <h1>Hocks in React</h1>
-//       <p>When we were using the class componet abd there would be a lot of inbuilt feature would there such as life cycle pure component if you want to use these the you have to use the <em>Hocks</em></p>
-//       <h3>Hocks can be used in the functional component</h3>
-//     </div>
-//   )
-// }
-
-export default App; 
+export default App;
