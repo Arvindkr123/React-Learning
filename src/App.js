5e846cdb8189ec906b4b805df8c2c468a55da847
Arvind <thakurarvindkr10@gmail.com>
2023-03-24 16:10:52 +0530

    Use memo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {PureComponent} from 'react'
-import User from './User';
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      count:0
-    }
-  }
+import React, { useState, useMemo} from 'react'
 
-  render(){
-    console.log('Check rerendering..............')
-    return (
-      <div className='App'>
-        {/* <h1>PureComponent in React {this.state.count}</h1> */}
-        <User count={this.state.count}></User>
-        {/* <button onClick={()=>this.setState({count:this.state.count+1})}>Update count</button> */}
-        <button onClick={()=>this.setState({count:1})}>Update count</button>
-      </div>
-    )
-  }
+function App() {
+  const[count, setCount] = useState(0);
+  const[item, setItem] = useState(10);
+  const multiCount = useMemo(()=>{
+    console.log('Use memo')
+    return count*5;
+  }, [count])
+  return (
+    <div className='App'>
+      <h1>Use Memo hoock in React</h1>
+      <h2>Count : {count}</h2>
+      <h2>Item : {item}</h2>
+      <h2>multiCount : {multiCount}</h2>
+      <button onClick={()=>setCount(count+1)}>Update count</button>
+      <button onClick={()=>setItem(item*10)}>Update item</button>
+    </div>
+  )
 }
 
 export default App
