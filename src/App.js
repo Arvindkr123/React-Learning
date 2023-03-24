bbeff696e8877f468f3dbea3bb03652a9820c616
Arvind <thakurarvindkr10@gmail.com>
2023-03-24 21:42:06 +0530

    controlled component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useRef } from 'react'
-import User from './User'
+import React, { useState } from 'react'
 
-function App(){
-  let inputRef = useRef(null)
-  function updateInput(){
-    inputRef.current.value='10000'
-    inputRef.current.style.color='red'
-    inputRef.current.style.backgroundColor='black'
-    inputRef.current.focus();
+/* Controlled Component it can have the input fields and it can be handled by the 
+   React State.
+ */
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      val: 1,
+      item: 34,
+    };
   }
-  return(
-    <div className='App'>
-      <h1>Forward Ref in React</h1>
-      <User ref={inputRef}/>
-      <button onClick={updateInput}>Update InputBoxValue</button>
-    </div>
-  )
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Controlled Component</h1>
+        <input type='text' defaultValue={'444'} onChange={(e) => this.setState({ val: e.target.value })} />
+        <input type='text' defaultValue={'000'} onChange={(e) => this.setState({ item: e.target.value })} />
+        <h3>value {this.state.val}</h3>
+        <h3>Item value {this.state.item}</h3>
+      </div>
+    );
+  }
 }
+// function App() {
+//   let [val, setVal] = useState()
+//   let [item, setItem] = useState()
+//   return (
+//     <div className='App'>
+//       <h1>Controlled Component</h1>
+//       <input type="text" defaultValue={'444'} onChange={(e)=>setVal(e.target.value)}/>
+//       <input type="text" defaultValue={'000'} onChange={(e)=>setItem(e.target.value)}/>
+//       <h3>value {val}</h3>
+//       <h3>Item value {item}</h3>
+//     </div>
+//   )
+// }
+// function App() {
+//   let [val, setVal] = useState("000")
+//   let [item, setItem] = useState("666")
+//   return (
+//     <div className='App'>
+//       <h1>Controlled Component</h1>
+//       <input type="text" value={val} onChange={(e)=>setVal(e.target.value)}/>
+//       <input type="text" value={item} onChange={(e)=>setItem(e.target.value)}/>
+//       <h3>value {val}</h3>
+//       <h3>Item value {item}</h3>
+//     </div>
+//   )
+// }
 
 export default App;
