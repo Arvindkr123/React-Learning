22f23f5630bbbc60f452256ef456538007be0fab
Arvind <thakurarvindkr10@gmail.com>
2023-03-24 22:22:32 +0530

    uncontrolled Component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react'
 
-/* Controlled Component it can have the input fields and it can be handled by the 
-   React State.
- */
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      val: 1,
-      item: 34,
-    };
-  }
+import React, { useRef } from 'react'
 
-  render() {
-    return (
-      <div className='App'>
-        <h1>Controlled Component</h1>
-        <input type='text' defaultValue={'444'} onChange={(e) => this.setState({ val: e.target.value })} />
-        <input type='text' defaultValue={'000'} onChange={(e) => this.setState({ item: e.target.value })} />
-        <h3>value {this.state.val}</h3>
-        <h3>Item value {this.state.item}</h3>
-      </div>
-    );
+function App() {
+  let inputRef = useRef(null)
+  let inputRef2 = useRef(null)
+  function submitForm(e) {
+    e.preventDefault();
+    console.warn(`input filed 1 value ${inputRef.current.value}`)
+    console.warn(`input filed 2 value ${inputRef2.current.value}`)
+    let input3 = document.getElementById('input3').value;
+    console.warn(`input filed 3 value ${input3}`)
   }
+  return (
+    <div className='App'>
+      <h1>Uncontrolled Component</h1>
+      <form onSubmit={submitForm}>
+        <input ref={inputRef} type="text" /><br /><br />
+        <input ref={inputRef2} type="text" /><br /><br />
+        <input id="input3" type="text" /><br /><br />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
 }
-// function App() {
-//   let [val, setVal] = useState()
-//   let [item, setItem] = useState()
-//   return (
-//     <div className='App'>
-//       <h1>Controlled Component</h1>
-//       <input type="text" defaultValue={'444'} onChange={(e)=>setVal(e.target.value)}/>
-//       <input type="text" defaultValue={'000'} onChange={(e)=>setItem(e.target.value)}/>
-//       <h3>value {val}</h3>
-//       <h3>Item value {item}</h3>
-//     </div>
-//   )
-// }
-// function App() {
-//   let [val, setVal] = useState("000")
-//   let [item, setItem] = useState("666")
-//   return (
-//     <div className='App'>
-//       <h1>Controlled Component</h1>
-//       <input type="text" value={val} onChange={(e)=>setVal(e.target.value)}/>
-//       <input type="text" value={item} onChange={(e)=>setItem(e.target.value)}/>
-//       <h3>value {val}</h3>
-//       <h3>Item value {item}</h3>
-//     </div>
-//   )
-// }
-
 export default App;
