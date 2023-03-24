03b9e46ee3480c8700cf97bd4eef1c272794142c
Arvind <thakurarvindkr10@gmail.com>
2023-03-24 08:04:26 +0530

    13_InstallBootStrap

--- src/App.js
+++ src/App.js
@@ -2,29 +2,65 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
+import Table from 'react-bootstrap/Table';
 import React from 'react'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Install BootStrap</h1>
-      {[
-        'primary',
-        'secondary',
-        'success',
-        'danger',
-        'warning',
-        'info',
-        'light',
-        'dark',
-      ].map((variant) => (
-        <Alert key={variant} variant={variant}>
-          This is a {variant} alert—check it out!
-        </Alert>
-      ))}
+      <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
     </div>
   )
 }
+// function App() {
+//   return (
+//     <div className='App'>
+//       <h1>Install BootStrap</h1>
+//       {[
+//         'primary',
+//         'secondary',
+//         'success',
+//         'danger',
+//         'warning',
+//         'info',
+//         'light',
+//         'dark',
+//       ].map((variant) => (
+//         <Alert key={variant} variant={variant}>
+//           This is a {variant} alert—check it out!
+//         </Alert>
+//       ))}
+//     </div>
+//   )
+// }
 // function App() {
 //   return (
 //     <div className='App'>
