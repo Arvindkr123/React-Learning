b1b76aacbcb91cfdf47ab93af9d04ce9c229eace
Arvind <thakurarvindkr10@gmail.com>
2023-03-25 23:14:57 +0530

    Added basic Routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as BR, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Navbar from './Navbar';
+import "./App.css";
 import React from 'react'
-import Page404 from './Page404';
-import Contact from './Contact';
-import User from './User';
-import Filter from './Filter';
-import Channels from './Channels';
-import Company from './Company';
-import Other from './Other';
-import Login from './Login';
-import Protected from './Protected';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
-      {/* <h1>Router in React</h1> */}
-      <BR>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Protected Component={Home} />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/about' element={<Protected Component={About} />} />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>Home Page</div>}/>
+      <Route path="/about" element={<div>About Page</div>}/>
+      <Route path="/contact" element={<div>Contact Page</div>}/>
+    </Routes>
 
-          <Route path='/contact/' element={<Protected Component={Contact} />}>
-            <Route path='channel' element={<Channels></Channels>} />
-            <Route path='company' element={<Company />} />
-            <Route path='other' element={<Other />} />
-          </Route>
-          <Route path='/user/:name' element={<User />} />
-          <Route path='/filter' element={<Protected Component={Filter} />} />
-          <Route path='/*' element={<Page404 />} />
-        </Routes>
-        {/* <Navbar/> */}
-      </BR>
-    </div>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
